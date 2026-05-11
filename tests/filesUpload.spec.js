90f45960d69dcdee7a180098dafba151fa597277
Ritik <your@email.com>
2026-05-11 10:56:35 +0530

    fix some errors

--- tests/filesUpload.spec.js
+++ tests/filesUpload.spec.js
@@ -4,7 +4,7 @@ test('Upload file from PC', async ({ page }) => {
 
     await page.goto('https://the-internet.herokuapp.com/upload');
 
-    await page.setInputFiles('#file-upload', 'test-data/uploads/example.png');
+    await page.setInputFiles('#file-upload', 'test-data/uploads/exampleimage.png');
 
     await page.click('#file-submit');
 
